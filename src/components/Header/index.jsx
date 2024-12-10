6583e672d94c8894b7dd8cfcc6b2859c4c8d8c4f
naved.uddin <naved.uddin@appsoln.com>
2024-12-10 19:48:04 +0530

    create header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,34 +1,35 @@
 import React from 'react';
 import { FaBell, FaUserCircle } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
+import styles from './Header.module.css';
 
 const index = () => {
     const navigate = useNavigate();
 
     return (
-        <header className="flex justify-between items-center bg-tertiary shadow-md p-4 font-semibold text-white">
-            <div className="flex items-center space-x-4">
+        <header className={styles["header-container"]}>
+            <div className={styles["logo-container"]}>
                 <img 
                     src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWMjR7gx6W5-B-hglc98RYENcZeIrSg0t6aA&s" 
-                    alt="LinkedIn Logo" 
-                    className="w-auto h-8"
+                    alt="CareerHive Logo" 
+                    className={styles["logo"]}
                 />
             </div>
 
-            <div className="flex-1 mx-4">
+            <div className={styles["search-container"]}>
                 <input 
                     type="text" 
                     placeholder="Search jobs, people, companies..."
-                    className="border-2 border-gray-300 p-2 rounded-lg focus:ring-2 focus:ring-blue-500 w-full text-black focus:outline-none"
+                    className={styles["searchbar"]}
                 />
             </div>
 
-            <div className="flex items-center space-x-6">
-                <p onClick={() => navigate('/')} className="hover:text-complementary cursor-pointer">Home</p>
-                <p onClick={() => navigate('/')} className="hover:text-complementary">Jobs</p>
-                <p onClick={() => navigate('/')} className="hover:text-complementary">Messages</p>
-                <p onClick={() => navigate('/')} className="hover:text-complementary">Notifications</p>
-                <p onClick={() => navigate('/')} className="hover:text-complementary">My Network</p>
+            <div className={styles["nav-container"]}>
+                <p onClick={() => navigate('/')} className={styles["nav-item"]}>Home</p>
+                <p onClick={() => navigate('/')} className={styles["nav-item"]}>Jobs</p>
+                <p onClick={() => navigate('/')} className={styles["nav-item"]}>Messages</p>
+                <p onClick={() => navigate('/')} className={styles["nav-item"]}>Notifications</p>
+                <p onClick={() => navigate('/')} className={styles["nav-item"]}>My Network</p>
             </div>
 
             <div className="flex items-center space-x-4 pl-2">
